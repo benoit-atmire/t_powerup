d159257048ad17d2cfbf49734f71c5b0f3815532
benoit <benoit@atmire.com>
2018-03-31 21:53:34 +0200

    First changes

--- js/client.js
+++ js/client.js
@@ -76,7 +76,11 @@ t.getAll();
 
 */
 
-
+var GLITCH_ICON = './images/glitch.svg';
+var WHITE_ICON = './images/icon-white.svg';
+var GRAY_ICON = './images/icon-gray.svg';
+var W2P_ICON = './images/icon-gray.svg';
+var GITLAB_ICON = './images/icon-gray.svg';
 
 var getBadges = function(t){
 
@@ -89,7 +93,7 @@ var getBadges = function(t){
           title: 'Detail Badge', // for detail badges only
           text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
           icon: GRAY_ICON, // for card front badges only
-          color: randomBadgeColor(),
+          color: 'red',
           refresh: 10 // in seconds
         };
       }
@@ -100,29 +104,6 @@ var getBadges = function(t){
       text: 'Static',
       icon: GRAY_ICON, // for card front badges only
       color: null
-    }, {
-      // card detail badges (those that appear on the back of cards)
-      // also support callback functions so that you can open for example
-      // open a popup on click
-      title: 'Popup Detail Badge', // for detail badges only
-      text: 'Popup',
-      icon: GRAY_ICON, // for card front badges only
-      callback: function(context) { // function to run on click
-        return context.popup({
-          title: 'Card Detail Badge Popup',
-          url: './settings.html',
-          height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
-        });
-      }
-    }, {
-      // or for simpler use cases you can also provide a url
-      // when the user clicks on the card detail badge they will
-      // go to a new tab at that url
-      title: 'URL Detail Badge', // for detail badges only
-      text: 'URL',
-      icon: GRAY_ICON, // for card front badges only
-      url: 'https://trello.com/home',
-      target: 'Trello Landing Page' // optional target for above url
     }];
   });
 };
